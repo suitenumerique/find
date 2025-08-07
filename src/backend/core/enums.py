89923e694b0ac941a34ba986cc0cdfeff7ddf5b1
Samuel Paccoud - DINUM <samuel.paccoud@numerique.gouv.fr>
2025-08-07 17:42:02 +0200

    ✨(schema) add fields to the document
    We need to index the tree structure information as well as an
    active field that can be set to False when the item is deleted on
    the remote service. We could delete the item from our search index
    but it is safer to keep all documents synchronized and not only
    those which are not deleted.

--- src/backend/core/enums.py
+++ src/backend/core/enums.py
@@ -16,12 +16,17 @@ class ReachEnum(str, Enum):
 # Fields
 
 CREATED_AT = "created_at"
+DEPTH = "depth"
+PATH = "path"
+NUMCHILD = "numchild"
 REACH = "reach"
 SIZE = "size"
 TITLE = "title"
 UPDATED_AT = "updated_at"
+USERS = "users"
+GROUPS = "groups"
 
 RELEVANCE = "relevance"
 
 ORDER_BY_OPTIONS = (RELEVANCE, TITLE, CREATED_AT, UPDATED_AT, SIZE, REACH)
-SOURCE_FIELDS = (TITLE, SIZE, CREATED_AT, UPDATED_AT, REACH)
+SOURCE_FIELDS = (TITLE, SIZE, DEPTH, PATH, NUMCHILD, CREATED_AT, UPDATED_AT, REACH)
