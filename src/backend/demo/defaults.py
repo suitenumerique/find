d575545832c2b7a989bfe597b5edcfe238ff1d8c
Fabre Florian <ffabre@hybird.org>
2025-10-06 16:02:10 +0200

    🔧(backend) setup Find app dockers to work with Docs
    Populate a service configuration "docs" for development.
    Move OIDC endpoints to the "impress" realm to allow introspection
    of Docs tokens.
    Upgrade version of dependencies (fix some security issues).
    
    Signed-off-by: Fabre Florian <ffabre@hybird.org>

--- src/backend/demo/defaults.py
+++ src/backend/demo/defaults.py
@@ -2,5 +2,9 @@
 
 NB_OBJECTS = {
     "documents": 1000,
-    "services": 5,
+    "services": 5
 }
+
+DEV_SERVICES = (
+    {"name": "docs", "client_id": "impress", "token": "find-api-key-for-docs-with-exactly-50-chars-length"},
+)
