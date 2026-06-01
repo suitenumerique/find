1fba76928a204440da7cc4b86ceac32b9075d794
Stephan Meijer <me@stephanmeijer.com>
2026-06-01 12:42:51 +0200

    fixup! 🔥(search) remove dead services parameter from search schema
    Signed-off-by: Stephan Meijer <me@stephanmeijer.com>

--- src/backend/demo/tests/test_commands_create_demo.py
+++ src/backend/demo/tests/test_commands_create_demo.py
@@ -27,11 +27,8 @@ def test_commands_create_demo(settings):
     call_command("create_demo")
 
     assert models.Service.objects.exclude(name="docs").count() == 4
-    indices = ",".join(
-        f"{settings.OPENSEARCH_INDEX_PREFIX}-{s.name}"
-        for s in models.Service.objects.all()
-    )
-    assert opensearch_client().count(index=indices)["count"] == 4
+    index_pattern = f"{settings.OPENSEARCH_INDEX_PREFIX}-*"
+    assert opensearch_client().count(index=index_pattern)["count"] == 4
 
     docs = models.Service.objects.get(name="docs")
     assert docs.client_id == "impress"
